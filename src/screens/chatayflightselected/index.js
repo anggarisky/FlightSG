a4bff460e55ea5748f5c27f24ec48a03714c2e60
Andre Pratama <andreepratama27@gmail.com>
2018-12-23 14:41:08 +0800

    fixing

--- src/screens/chatayflightselected/index.js
+++ src/screens/chatayflightselected/index.js
@@ -1,32 +1,28 @@
-import React, { Component } from "react";
-import { View, Text, Image, ScrollView } from "react-native";
-import { Button, Input, Item } from "native-base";
-import styles from "./styles";
+import React, {Component} from 'react';
+import {View, Text, Image, ScrollView} from 'react-native';
+import {Button, Input, Item} from 'native-base';
+import styles from './styles';
 
 class Chatayflightselected extends Component {
-
-  navigateTo = data => {
-
-    this.props.navigation.navigate("Chatayflighthomes", {
-        data
-      });
-    
+  navigateTo = (pages, data) => {
+    this.props.navigation.navigate('Chatayflighthomes', {
+      data,
+    });
   };
 
   render() {
-    const { getParam } = this.props.navigation;
-    const { arrival, departure } = getParam("data");
+    const {getParam} = this.props.navigation;
+    const {arrival, departure} = getParam('data');
 
     return (
       <View>
         <View style={styles.mainbody}>
           <Button
-            onPress={() => this.props.navigation.navigate("Chatayflightselect")}
-            style={{ backgroundColor: "none" }}
-          >
+            onPress={() => this.props.navigation.navigate('Chatayflightselect')}
+            style={{backgroundColor: 'none'}}>
             <Image
-              style={{ height: 50, width: 50 }}
-              source={require("../../assets/imgbtnback.png")}
+              style={{height: 50, width: 50}}
+              source={require('../../assets/imgbtnback.png')}
             />
           </Button>
           <Image
@@ -35,9 +31,9 @@ class Chatayflightselected extends Component {
               marginBottom: 30,
               marginLeft: 30,
               height: 100,
-              width: 260
+              width: 260,
             }}
-            source={require("../../assets/chataylogo.png")}
+            source={require('../../assets/chataylogo.png')}
           />
           <Text
             style={{
@@ -45,27 +41,27 @@ class Chatayflightselected extends Component {
               lineHeight: 30,
               marginLeft: 70,
               width: 180,
-              color: "#707070",
-              fontWeight: "bold",
+              color: '#707070',
+              fontWeight: 'bold',
               fontSize: 22,
-              textAlign: "center"
-            }}
-          >
-            Cathay Pacific CX{arrival}/{departure}{" "}
+              textAlign: 'center',
+            }}>
+            Cathay Pacific CX{arrival}/{departure}{' '}
           </Text>
 
           <Button
-            onPress={() => this.navigateTo(arrival)}
+            onPress={() => this.navigateTo('Chatayflighthomes', arrival)}
             block
             light
-            style={styles.buttoncta}
-          >
+            style={styles.buttoncta}>
             <Text style={styles.textbuttoncta}>CX{arrival} ARRIVAL</Text>
           </Button>
 
-          <Button 
-          onPress={() => this.navigateTo(departure)}
-          block light style={styles.buttoncta}>
+          <Button
+            onPress={() => this.navigateTo('Chatayflighthomesdept', departure)}
+            block
+            light
+            style={styles.buttoncta}>
             <Text style={styles.textbuttoncta}>CX{departure} DEPARTURE</Text>
           </Button>
         </View>
